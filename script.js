a850541af4684377093b79b52d8c71a8dd95e638
YuriyBiakov <gbyackov@gmail.com>
2024-01-22 20:48:56 +0300

    feat: add script code (not finished)

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 'use strict'
+
+const inputForm = document.getElementById('input-form'); // FORM el
+const formInput = document.getElementById('text-input'); // INPUT el
+// we can get value from INPUT element< not FORM !!!
+let userInputValue;
+const resultText = document.getElementById('result')
+const resultStrongText = document.getElementById('user-input-text');
+
+
+function getUserInputData(currentEvent){
+    currentEvent.preventDefault();
+    const userInput = formInput.value;
+    
+    if (userInput === ''){
+        alert('Please input a value');
+    } else {
+        userInputValue = userInput;
+        console.log(userInputValue);
+        cleanString(userInputValue);
+        cleanInput();
+        resultStrongText.innerText = userInputValue;
+        resultText.setAttribute('display','block');
+    }
+}
+
+function cleanInput() {
+    formInput.value = '';
+}
+
+function cleanString(str) {
+    console.log(str);
+    const regex = /[\w]/g;
+    let result = str.toString();
+    result = result.match(regex);
+    result = result.join('');
+    result = result.replace(/[\_]/g,'');
+    console.log('cleaned string = ' + result);
+    return result;
+}
+
+function isPalindrome (string){
+    string = string.toLowerCase();
+    
+
+}
+
+inputForm.addEventListener('submit', getUserInputData);
